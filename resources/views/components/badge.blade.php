56cf4413fcbc6d3ded7dfb4622bd529a2261c1ec
llayz46 <layzer@outlook.fr>
2024-10-25 21:13:49 +0200

    Adding background on badges "genre"

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,5 +1,5 @@
 @props(['tag'])
 
-<{{ $tag }} class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium shadow text-neutral-400 ring-1 ring-inset ring-background-accent-hover cursor-default">
+<{{ $tag }} class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium shadow text-neutral-400 ring-1 backdrop-blur-[2px] bg-background-accent-hover/25 ring-inset ring-background-accent-hover cursor-default">
 {{ $slot }}
 </{{ $tag }}>
