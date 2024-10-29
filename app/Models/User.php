8259d802af561c32445a4cc4c3a00241dac063c6
llayz46 <layzer@outlook.fr>
2024-10-29 10:47:35 +0100

    Fix banner for level up

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Jetstream\InteractsWithBanner;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
@@ -22,7 +21,6 @@ class User extends Authenticatable implements MustVerifyEmail
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
-    use InteractsWithBanner;
 
     /**
      * The attributes that are mass assignable.
@@ -149,7 +147,6 @@ class User extends Authenticatable implements MustVerifyEmail
         foreach ($this->levels as $level => $xpRequired) {
             if ($this->xp >= $xpRequired) {
                 $this->level = $level;
-                $this->banner('Bravo ! Vous avez atteint le niveau ' . $level . ' !');
             }
         }
     }
