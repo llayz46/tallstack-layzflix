79cd2d407ca51714b5ca7e49008839271a462715
llayz46 <layzer@outlook.fr>
2024-11-15 17:43:35 +0100

    Feature: Adding filters on playlist page

--- app/Livewire/Playlist/Show.php
+++ app/Livewire/Playlist/Show.php
@@ -5,6 +5,7 @@ namespace App\Livewire\Playlist;
 use App\Models\Media;
 use App\Models\Playlist;
 use App\Models\User;
+use Illuminate\Support\Collection;
 use Laravel\Jetstream\InteractsWithBanner;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -16,6 +17,7 @@ class Show extends Component
 
     public Playlist $playlist;
     public User $user;
+    public string $filter = '';
 
     public function delete(Media $media)
     {
@@ -26,13 +28,45 @@ class Show extends Component
         $this->banner($media->normalized_title . ' à bien été retiré de la playlist');
     }
 
+    public function filtering($filter)
+    {
+        $this->filter = $filter;
+    }
+
+    public function updatedFilter()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
         $this->authorize('view', $this->playlist);
 
+        $medias = $this->playlist->medias();
+
+        if(!empty($this->filter)) {
+            if($this->filter === 'release_date') {
+                $medias = $medias->orderBy('release_date', 'desc');
+            } elseif($this->filter === 'popular') {
+                $medias = $medias->withCount('reviews')
+                    ->get()
+                    ->sortByDesc('reviews_count');
+            } elseif($this->filter === 'rating') {
+                $medias = $medias->withCount('reviews')
+                    ->with('reviews')
+                    ->get()
+                    ->sortByDesc(function($media) {
+                        return [
+                            $media->reviews->avg('rating'),
+                            $media->reviews_count
+                        ];
+                    });
+            }
+        }
+
         return view('livewire.playlist.show', [
             'playlist' => $this->playlist,
-            'medias' => $this->playlist->medias()->paginate(10),
+            'medias' => $medias->paginate(10),
         ])
             ->layout('layouts.app')
             ->title($this->user->username . ' - ' . $this->playlist->name . ' - LayzFlix');
--- resources/views/livewire/playlist/show.blade.php
+++ resources/views/livewire/playlist/show.blade.php
@@ -8,6 +8,42 @@
             <x-button type="secondary" class="mt-auto" href="{{ route('profile', $user->slug) }}">Retourner au profile</x-button>
         </x-slot:button>
 
+        <div class="flex items-center justify-between mb-5">
+            <div class="relative inline-block text-left" x-data="{ filterBy: false }">
+                <div>
+                    <button type="button" class="group inline-flex justify-center text-sm font-medium text-gray-300 hover:text-white"
+                            aria-expanded="false" aria-haspopup="true" @click="filterBy = ! filterBy">
+                        Filtrer par
+                        <svg class="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-300 group-hover:text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+
+                <div class="absolute left-0 z-30 mt-2 w-40 origin-top-left rounded-md bg-background-accent shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none"
+                     role="menu" aria-orientation="vertical" aria-labelledby="mobile-menu-button" tabindex="-1"
+                     x-show="filterBy"
+                     x-transition:enter="transition ease-out duration-100"
+                     x-transition:enter-start="transform opacity-0 scale-95"
+                     x-transition:enter-end="transform opacity-100 scale-100"
+                     x-transition:leave="transition ease-in duration-75"
+                     x-transition:leave-start="transform opacity-100 scale-100"
+                     x-transition:leave-end="transform opacity-0 scale-95" x-cloak>
+                    <div class="py-1" role="none">
+                        <form wire:submit.prevent="filtering('popular')" method="post">
+                            <button type="submit" class="block px-4 py-2 text-sm font-medium text-gray-300 hover:bg-background-accent-hover w-full text-start" role="menuitem" tabindex="-1">Les plus populaires</button>
+                        </form>
+                        <form wire:submit.prevent="filtering('rating')" method="post">
+                            <button type="submit" class="block px-4 py-2 text-sm font-medium text-gray-300 hover:bg-background-accent-hover w-full text-start" role="menuitem" tabindex="-1">Les mieux notés</button>
+                        </form>
+                        <form wire:submit.prevent="filtering('release_date')" method="post">
+                            <button type="submit" class="block px-4 py-2 text-sm font-medium text-gray-300 hover:bg-background-accent-hover w-full text-start" role="menuitem" tabindex="-1">Les plus récents</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="grid gap-x-6 gap-y-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-5">
             @if($medias->isEmpty())
                 <div class="col-span-5">
